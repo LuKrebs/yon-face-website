5d4fa66757abb4dbaeb8543f7e90d266deb7916a
Luciano <luciano.krebs@hotmail.com>
2017-06-01 22:35:05 -0300

    starting a new section

--- js/script.js
+++ js/script.js
@@ -8,8 +8,42 @@ $(document).ready(function(){
 	$("#first").hide();
 	$("#second").hide();
 
-	$("#first").fadeIn(2000);
-	$("#second").fadeIn(2500);
-	
+	$("#sfirst").hide();
+	$("#ssecond").hide();
 
+	$("#camera").hide();
+
+	// make the text appear and disappear 
+	setTimeout(function() {
+	  $("#first").fadeIn(1500);
+	}, 1500);
+
+	setTimeout(function() {
+	  $("#second").fadeIn(2000);
+	}, 3500);
+
+	setTimeout(function() {
+	  $("#sfirst").fadeIn(1000);
+	}, 7000);
+
+	setTimeout(function() {
+	  $("#ssecond").fadeIn(1000);
+	}, 10000);
+
+	setTimeout(function() {
+	  $("#camera").fadeIn(1500);
+	}, 1500);
+
+	/* $("#testFont").each(function() {
+		var e = $(this);
+		setInterval(function() {
+			if (e.css('visibility') == 'hidden') {
+				e.css('visibility', 'visible');
+			}
+			else {
+				e.css('visibility', 'hidden');	
+			}
+		}, 1000);
+	});
+	*/ 
 });
